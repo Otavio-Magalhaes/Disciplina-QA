6e82e499d4863c2d83ffc756e5ccc77f5c24609b
Otavio <103613657+Otavio-Magalhaes@users.noreply.github.com>
2025-03-14 21:33:27 -0300

    Frist exercise with checkStyle.IDEA

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,32 @@
+
+
+public final class Calculadora {
+    /**@param n1 valor 1
+     * @param n2 valor 2
+     * @return retorna o resultado da operação
+     */
+    public double somar(final double n1, final double n2) {
+        return  n1 + n2;
+    }
+    /**@param n1 valor 1
+     * @param n2 valor 2
+     * @return retorna o resultado da operação
+     */
+    public double subtrair(final double n1, final double n2) {
+        return  n1 - n2;
+    }
+    /**@param n1 valor 1
+     * @param n2 valor 2
+     * @return retorna o resultado da operação
+     */
+    public double multiplicar(final double n1, final double n2) {
+        return  n1 * n2;
+    }
+    /**@param n1 valor 1
+     * @param n2 valor 2
+     * @return retorna o resultado da operação
+     */
+    public double dividir(final double n1, final double n2) {
+        return  n1 / n2;
+    }
+}
